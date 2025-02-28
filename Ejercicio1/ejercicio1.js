37395b29664b7d27ce7ed7f921e553b2c0f91d80
LilJulian <juliadar267@gmail.com>
2025-02-28 11:30:48 -0500

    ejercicio 3

---
+++ Ejercicio1/ejercicio1.js
@@ -0,0 +1,36 @@
+//Usando la funcion set timeout
+
+// function imprimirNumeros(desde, hasta) {
+//   setTimeout(() => {
+//     if (desde <= hasta) {
+//       console.log(desde++);
+//       imprimirNumeros(desde, hasta);
+//     }
+//   }, 1000);
+// }
+
+
+// let anidado = setTimeout(function imprimirNumeros(desde, hasta) {
+//   if (desde <= hasta) {
+//     console.log(desde++);
+//   }
+//   anidado = setTimeout(imprimirNumeros(desde, hasta), 1000);
+// }, 1000)
+
+
+// console.log(anidado);
+
+// console.log(temporizador);
+
+
+
+// function imprimirNumeros(desde, hasta) {
+//   setInterval(() => {
+//     if (desde <= hasta) {
+//       console.log(desde++);
+//       imprimirNumeros(desde, hasta);
+//     }
+//   }, 1000);
+// }
+
+// imprimirNumeros(1, 6);
---
+++ Ejercicio3/ejercicio3.js
@@ -0,0 +1,25 @@
+// 3. Crear una función map que acepte un array y un callback y que:
+//     • por cada elemento del array ejecute el callback pasándole dicho elemento como argumento
+//     • obtenga el resultado
+//     • lo pushee a un nuevo array
+//     • devuelva el array final con el resultado de cada una de las llamadas al callback.
+
+function map(arreglo, callback) {
+  let nuevoArreglo = []
+  for (let i = 0; i < arreglo.length; i++) {
+    num = arreglo[i];
+    console.log(nuevoArreglo);
+
+  }
+
+
+}
+
+function callback(resultado, nuevoArreglo) {
+  for (let j = 0; j < arreglo.length; j++) {
+    nuevoArreglo[i].push(i)
+  }
+  callback(num);
+}
+
+map([1, 2, 3, 5], callback);
