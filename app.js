cc399988c1d6f7f60c996fb4680f5b92304182e8
LilJulian <juliadar267@gmail.com>
2025-02-28 08:00:32 -0500

    Inicio del taller

---
+++ app.js
@@ -0,0 +1,54 @@
+// let funcion1 = () => console.log("Se ejecuto la funcion 1");
+
+// function funcion2() {
+//   console.log("Se ejecuto la funcion 2");
+// }
+
+// funcion1();
+// setTimeout(funcion2, 3000);
+
+// const avanzar = setInterval(() => {
+//   funcion1();
+// }, 2000);
+
+// const detener = setTimeout(() => {
+//   clearInterval(avanzar);
+// }, 6000);
+
+// console.log(avanzar);
+// console.log(detener);
+
+
+{
+  // function nombre(nombre) {
+  //   console.log(`Bienvenido ${nombre}, ingrese los siguientes datos y espere la confirmacion`);
+  //   return nombre;
+
+  // }
+  // function datos(telefono, edad, nombre) {
+  //   console.log(`espere por favor`);
+  //   const guardar = setTimeout(() => {
+  //     console.log(`telefono: ${telefono} y edad ${edad}.
+  //     Sus datos han sido confirmados`);
+
+  //   }, 3000);
+  // }
+  // datos(30012, 18, nombre("Julian"));
+}
+
+{
+  // function animal(especie) {
+  //   console.log("Es un " + especie);
+  // }
+
+  // function especimen(callback) {
+  //   var especie = prompt("Ingrese la especie");
+  //   callback(especie);
+
+  // }
+
+  // especimen(animal);
+}
+
+
+
